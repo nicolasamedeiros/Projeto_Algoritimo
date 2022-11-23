6b153eaeb9f86c108b566363911ff4f3979c9f86
marcosgs <mvinicius17112002@gmail.com>
2022-11-23 20:29:40 -0300

    commit

--- Projeto_Final.c
+++ Projeto_Final.c
@@ -1,5 +1,38 @@
-//Nicolas Almeida - UC22103171 - Engenharia de software / Marcos Vinicius Gonçalves da Silva -  - Engenharia de Software / Ronaldo Cavalcante Vicente -  - Engenharia de Software
+//Nicolas Almeida - UC22103171 - Engenharia de software / Marcos Vinicius Gonçalves da Silva - UC22200517 - Engenharia de Software / Ronaldo Cavalcante Vicente - UC22200227  - Engenharia de Software
 
-int main () {
-    const cheia = 50, meia = 25;
-}
+#include<stdio.h>
+    int main (){
+        int sessao,qdapessoas,sexoM;
+        const cheia = 50, meia = 25;
+          do
+          { printf("Digite a quantidade de sessoes: ");
+           scanf("%d",&sessao);
+            system("cls");
+          } while (sessao != 2);
+
+          for(){
+
+             do
+            {   printf("Digite a quantidade de pessoas que assistiram o filme: ");
+                scanf("%d",&qdapessoas);
+                system("cls");
+            } while (qdapessoas < 10); 
+
+            for(qdapessoas){
+
+            }
+
+
+
+          }
+            
+           
+             
+         
+            
+            
+            
+          
+        
+            
+            }
