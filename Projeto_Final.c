0f8cdd76677552203ab80e0f98c14183eb971c7a
Nicolas Almeida Medeiros <61924838+nicolasamedeiros@users.noreply.github.com>
2022-11-23 20:22:12 -0300

    Projeto_Final

---
+++ Projeto_Final.c
@@ -0,0 +1,5 @@
+//Nicolas Almeida - UC22103171 - Engenharia de software / Marcos Vinicius Gonçalves da Silva -  - Engenharia de Software / Ronaldo Cavalcante Vicente -  - Engenharia de Software
+
+int main () {
+    const cheia = 50, meia = 25;
+}
