13a6649343ef60c8dfa9eeae9d392c5fa896f460
nicolasamedeiros <61924838+nicolasamedeiros@users.noreply.github.com>
2022-11-27 12:35:05 -0300

    Fiz a questão 3 e falta terminar a 4

--- Projeto_Final.c
+++ Projeto_Final.c
@@ -1,38 +1,33 @@
 //Nicolas Almeida - UC22103171 - Engenharia de software / Marcos Vinicius Gonçalves da Silva - UC22200517 - Engenharia de Software / Ronaldo Cavalcante Vicente - UC22200227  - Engenharia de Software
 
 #include<stdio.h>
-    int main (){
-        int sessao,qdapessoas,sexoM;
-        const cheia = 50, meia = 25;
-          do
-          { printf("Digite a quantidade de sessoes: ");
-           scanf("%d",&sessao);
-            system("cls");
-          } while (sessao != 2);
+#include<stdlib.h>
+#include<string.h>
 
-          for(){
-
-             do
-            {   printf("Digite a quantidade de pessoas que assistiram o filme: ");
-                scanf("%d",&qdapessoas);
-                system("cls");
-            } while (qdapessoas < 10); 
-
-            for(qdapessoas){
-
-            }
-
-
-
-          }
-            
-           
-             
-         
-            
-            
-            
+int main (){
+  int sessao, qdapessoas, sexo = 0, idade;
+  const cheia = 50, meia = 25;
           
-        
-            
-            }
+  do
+  { 
+    printf("Digite a quantidade de sessoes: ");
+    scanf("%d",&sessao);
+    system("cls");
+  } while (sessao != 2);
+
+  do 
+  {
+    printf("Digite a quantidade de pessoas na sessao: ");
+    scanf("%d", &qdapessoas);
+    system("cls");
+  } while(qdapessoas < 10);   
+
+  for (int i = 0; i <= qdapessoas; i++) {
+    do
+    {
+      printf("Digite o sexo do cliente: ");
+      scanf("%c", &sexo);
+    } while (sexo == 'M' || sexo == 'F');
+  }
+                       
+}
