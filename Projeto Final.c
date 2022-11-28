13beb12dde63338ddf53801c24f0698960866e21
nicolasamedeiros <61924838+nicolasamedeiros@users.noreply.github.com>
2022-11-28 20:24:20 -0300

    Nova att

--- Projeto Final.c
+++ Projeto Final.c
@@ -4,18 +4,19 @@
 #include<locale.h>
 
 int main (){
-  int sessao, qdapessoas, sexo, sexoM = 0, sexoF = 0, idade, i,ingresso,custoTotal=0,count=1,crianca=0,adolescente=0,adulto=0,idoso=0;
-  
+  int sessao, qdapessoas, sexoM = 0, sexoF = 0, idade, i,ingresso,custoTotal=0,count=1,crianca=0,adolescente=0,adulto=0,idoso=0;
+  char sexo;
+
   setlocale(LC_ALL, "Portuguese");
 
   do
-  { 
+  {
     printf("Digite a quantidade de sessoes: ");
     scanf("%d",&sessao);
     system("cls");
   } while (sessao != 2);
 
-  do 
+  do
   {
     printf("Digite a quantidade de pessoas na sessao: ");
     scanf("%d", &qdapessoas);
@@ -23,38 +24,38 @@ int main (){
   } while(qdapessoas < 10);
 
     for(i=0;i<qdapessoas;i++){
+      idade = 0;
+      printf("Qual o sexo do %d° cliente: ",i+1);
+      fflush(stdin);
+      scanf("%c", &sexo);
 
-      for(int j = 0;j<qdapessoas;j++) {
-        printf("Qual o sexo do %d° cliente: ",count);
-        fflush(stdin);
-        scanf("%c", &sexo);
-        count++;
+      if (sexo == 'M' || sexo =='m') {
+        sexoM = sexoM + 1;
+        do{
+          printf("Qual a idade do %d° cliente: ",count);
+          scanf("%d", &idade);
+        }while(idade<=3 && idade>=100);
       }
+      else if (sexo == 'F' || sexo =='f') {
+        sexoF =  sexoF + 1;
+        do{
+          printf("Qual a idade do %d° cliente: ",i+1);
+          scanf("%d", &idade);
+        }while(idade<=3 && idade>=100);
 
-    if (sexo == 'M' || sexo =='m') {
-      sexoM = sexoM + 1;
-      do{
-        printf("Qual a idade do %d° cliente: ",count);
-        scanf("%d", &idade);
-      }while(idade>=3 && idade<=100);
+            if(idade>=3 && idade<14){
+                crianca = crianca + 1;
+            } else if(idade >= 14 && idade < 18){
+                adolescente = adolescente + 1;
+            } else if(idade >= 18 && idade < 65){
+                adulto = adulto + 1;
+            } else {
+                idoso = idoso + 1;
+            }
+        }
     }
-    else if (sexo == 'F' || sexo =='f') {
-      sexoF =  sexoF + 1;
-      do{
-        printf("Qual a idade do %d° cliente: ",count);
-        scanf("%d", &idade);
-      }while(idade>=3 && idade<=100);
-    }
-    
-    if(idade>=3 && idade<14){
-        	crianca = crianca + 1;
-		} else if(idade >= 14 && idade < 18){
-			adolescente = adolescente + 1;
-		} else if(idade >= 18 && idade < 65){
-			adulto = adulto + 1;
-		} else {
-			idoso = idoso + 1;
-		}
+
+
 
     do{
       printf("O ingresso sera meia ou inteira (1)Meia (2)Inteira : ");
@@ -68,7 +69,6 @@ int main (){
     }
     count++;
 
-}
 	printf("\nSexo Feminino = %d",sexoF);
 	printf("\nSexo Masculino = %d",sexoM);
 	printf("\nCrianças = %d",crianca);
