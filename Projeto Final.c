530e3a91022dca0e8ce10efdd574441785b19f5c
nicolasamedeiros <61924838+nicolasamedeiros@users.noreply.github.com>
2022-11-28 16:05:44 -0300

    Arrumei o problema de só perguntar o sexo uma vez
    Arrumei o problema de só perguntar o sexo uma vez e arrumei os operadores que estavam errados com base na questão 6

--- Projeto Final.c
+++ Projeto Final.c
@@ -24,41 +24,44 @@ int main (){
 
     for(i=0;i<qdapessoas;i++){
 
-      printf("Qual o sexo do %d° cliente: ",count);
-      fflush(stdin);
-      scanf("%c", &sexo);
+      for(int j = 0;j<qdapessoas;j++) {
+        printf("Qual o sexo do %d° cliente: ",count);
+        fflush(stdin);
+        scanf("%c", &sexo);
+        count++;
+      }
 
-    if (sexo == 'M' || sexo=='m') {
-          sexoM = sexoM + 1;
-        do{
+    if (sexo == 'M' || sexo =='m') {
+      sexoM = sexoM + 1;
+      do{
         printf("Qual a idade do %d° cliente: ",count);
         scanf("%d", &idade);
-        }while(idade<3 && idade>100);
+      }while(idade>=3 && idade<=100);
     }
     else if (sexo == 'F' || sexo =='f') {
       sexoF =  sexoF + 1;
-        do{
+      do{
         printf("Qual a idade do %d° cliente: ",count);
         scanf("%d", &idade);
-        }while(idade<3 && idade>100);
+      }while(idade>=3 && idade<=100);
     }
     
     if(idade>=3 && idade<14){
         	crianca = crianca + 1;
-		} else if(idade <18){
+		} else if(idade >= 14 && idade < 18){
 			adolescente = adolescente + 1;
-		} else if(idade <65){
+		} else if(idade >= 18 && idade < 65){
 			adulto = adulto + 1;
 		} else {
 			idoso = idoso + 1;
 		}
 
     do{
-    printf("O ingresso sera meia ou inteira (1)Meia (2)Inteira : ");
-    scanf("%d", &ingresso);
-    } while(ingresso!= 1 && ingresso!=2);
+      printf("O ingresso sera meia ou inteira (1)Meia (2)Inteira : ");
+      scanf("%d", &ingresso);
+    } while(ingresso != 1 || ingresso !=2);
 
-    if(ingresso==1){
+    if(ingresso == 1){
         custoTotal = custoTotal + 25;
     } else{
         custoTotal = custoTotal + 50;
