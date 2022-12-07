32c0bdd3d49bb9320b7649a0abf9475f84f456f1
marcosgs <mvinicius17112002@gmail.com>
2022-12-07 11:23:16 -0300

    atl

--- Projeto Final.c
+++ Projeto Final.c
@@ -1,5 +1,4 @@
-//Nicolas Almeida - UC22103171 - Engenharia de software / Marcos Vinicius Gonçalves da Silva - UC22200517 - Engenharia de Software / Ronaldo Cavalcante Vicente - UC22200227  - Engenharia de Software / Erik daniel Silva de Souza - UC22200407
-
+//Nicolas Almeida - UC22103171 - Engenharia de software / Marcos Vinicius Gonçalves da Silva - UC22200517 - Engenharia de Software / Ronaldo Cavalcante Vicente - UC22200227  - Engenharia de Software / Erik Daniel Silva de Souza - UC22200407
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
@@ -8,19 +7,14 @@
 int main (){
   int sessao, qdapessoas, sexoM = 0, sexoF = 0, idade, i,ingresso,custoTotal=0,count=1,crianca=0,adolescente=0,adulto=0,idoso=0,quantidadepipoca, tamanho = 0, l, k, j, qntPessoasSessao=1;
   const pipocaP = 10, pipocaM = 15, pipocaG = 20;
-  float valorpipoca = 0.0, vpTotal;
+  float valorpipoca = 0.0;
   char sexo;
   
   setlocale(LC_ALL, "Portuguese");
-  
-  printf("Seja bem vindo!\n");
-  system("pause");
-  system("cls");
-  
 
   do
   { 
-    printf("Por gentileza digite a quantidade de sess?es: ");
+    printf("Digite a quantidade de sessoes: ");
     scanf("%d",&sessao);
     system("cls");
   } while (sessao != 2);
@@ -29,72 +23,114 @@ int main (){
 
   do 
   {
-    printf("Querido cliente digite a quantidade de pessoas na %d? sessao: ",qntPessoasSessao);
+    printf("Digite a quantidade de pessoas na %d° sessao: ",qntPessoasSessao);
     scanf("%d", &qdapessoas);
     system("cls");
   } while(qdapessoas < 3);
 
     for(i=0;i<qdapessoas;i++){
 
-      printf("Qual o sexo do %d? cliente (M) Masculino (F) Feminino: ",count);
+      printf("Qual o sexo do %d° cliente (M) Masculino (F) Feminino: ",count);
       fflush(stdin);
       scanf("%c", &sexo);
 
-    if (sexo == 'M' || sexo=='m'){
+    if (sexo == 'M' || sexo=='m') {
           sexoM = sexoM + 1;
         do{
-          printf("Qual a idade do %d° cliente: ",count);
-          scanf("%d", &idade);
-        }while(idade<=3 && idade>=100);
-      }
-      else if (sexo == 'F' || sexo =='f') {
-        sexoF =  sexoF + 1;
-        do{
-          printf("Qual a idade do %d° cliente: ",i+1);
-          scanf("%d", &idade);
-        }while(idade<=3 && idade>=100);
-
-            if(idade>=3 && idade<14){
-                crianca = crianca + 1;
-            } else if(idade >= 14 && idade < 18){
-                adolescente = adolescente + 1;
-            } else if(idade >= 18 && idade < 65){
-                adulto = adulto + 1;
-            } else {
-                idoso = idoso + 1;
-            }
-            
-        }
+        printf("Qual a idade do %d° cliente: ",count);
+        scanf("%d", &idade);
+        }while(idade<3 && idade>100);
     }
-
-
+    else if (sexo == 'F' || sexo =='f') {
+      sexoF =  sexoF + 1;
+        do{
+        printf("Qual a idade do %d° cliente: ",count);
+        scanf("%d", &idade);
+        }while(idade<3 && idade>100);
+    }
+    
+    if(idade>=3 && idade<14){
+        	crianca = crianca + 1;
+		} else if(idade <18){
+			adolescente = adolescente + 1;
+		} else if(idade <65){
+			adulto = adulto + 1;
+		} else {
+			idoso = idoso + 1;
+		}
 
     do{
-      printf("O ingresso sera meia ou inteira (1)Meia (2)Inteira : ");
-      scanf("%d", &ingresso);
-    } while(ingresso != 1 || ingresso !=2);
+    printf("O ingresso sera meia ou inteira (1)Meia (2)Inteira : ");
+    scanf("%d", &ingresso);
+    system("cls");
+    } while(ingresso!= 1 && ingresso!=2);
 
-    if(ingresso == 1){
+    if(ingresso==1){
         custoTotal = custoTotal + 25;
-      } else{
+    } else{
         custoTotal = custoTotal + 50;
-      }
     }
     count++;
+}
 
-	printf("Dados da sessao\n\n");
+count=1;
+
+        for (i=0; i<qdapessoas; i++){
+            
+            printf("Informe quantas pipocas o %d° Cliente deseja: ", count);
+            scanf("%d", &quantidadepipoca);
+            
+            for(k=0;k<quantidadepipoca;k++){
+            printf("Escolha o tamanho da pipoca (1)pequena, (2)Média, (3)Grande para a pessoa %d: ", k);
+            scanf("%d", &tamanho);
+            
+            switch(tamanho)
+            {
+                case 1 :
+                {
+                    valorpipoca =quantidadepipoca * pipocaP;
+                    
+                    break;
+                }
+
+                case 2 :
+                {
+                    valorpipoca = quantidadepipoca * pipocaM;
+                    
+                    break;
+                }
+
+                case 3 :
+                {
+                    valorpipoca = quantidadepipoca * pipocaG;
+                    
+                    break;
+                }
+
+                default:
+                {
+                    printf("\nNumero invalido!\n");
+                }
+            }
+          }
+
+            
+    count++; system("cls");system("pause");
+  } 
+
+
 	printf("\nSexo Feminino = %d",sexoF);
 	printf("\nSexo Masculino = %d",sexoM);
-	printf("\nCrian?as = %d",crianca);
+	printf("\nCrianças = %d",crianca);
 	printf("\nAdolescentes = %d",adolescente);
 	printf("\nAdulto = %d",adulto);
-	printf("\nIdoso = %d",idoso);
-	printf("\nValor Total =  %d\n",custoTotal);
+	printf("\nIdoso = %d\n\n",idoso);
+	
+  system("pause");
 
-	system("pause");
   
 
   qntPessoasSessao++;
   system("cls");
-}
+  }
 }
